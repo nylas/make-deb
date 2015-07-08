52faba85896e2a93b70533e9b76e814f9a427638
Rob McQueen <rob@nylas.com>
2015-07-08 10:14:31 -0700

    Advancing to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='make-deb',
-    version='0.0.1',
+    version='0.0.2',
     include_package_data = True,
     packages = ['make_deb'],
     author = "Rob McQueen",

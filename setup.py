4478a056df904eed56a2de72c8f34e4b2d0c2ddb
Rob McQueen <rob@nylas.com>
2015-07-11 11:57:17 -0700

    0.0.3 => 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='make-deb',
-    version='0.0.3',
+    version='0.0.4',
     include_package_data = True,
     packages = ['make_deb'],
     author = "Rob McQueen",
